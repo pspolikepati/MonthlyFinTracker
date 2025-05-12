454ed270faf90de3d289a74770f6caaca78f920a
pspolikepati <145732198+pspolikepati@users.noreply.github.com>
2025-05-12 10:09:09 -0500

    Completed the daysSinceStart and totalDaysInMonth boxes, made two more screens with relevant images, and implemented the navigation button with the logic of which screen to choose to display based on comparison of expenses to budget ratio to days since start of the month to total days in the month ratio

--- app/index.tsx
+++ app/index.tsx
@@ -1,74 +1,6 @@
 import React, { Component } from 'react';
-import { Image, ScrollView, StyleSheet, Text, TouchableHighlight, View, TextInput } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, TouchableHighlight, View, TextInput,Button } from 'react-native';
 import CurrencyInput from 'react-currency-input-field';
-
-/* const song = [
-    {
-        id: '1',
-        title: "Photograph",
-        artist: 'Ed Sheeran',
-        artistPic: 'https://codehs.com/uploads/1128f7b2a2b686cf82e14612bb291490',
-        duration: '4:18',
-        image: 'https://codehs.com/uploads/71466ceae7bd24e3e6b52d0c793e36de'
-    },
-    {
-        id: '2',
-        title: "Thinking Out Loud",
-        artist: 'Ed Sheeran',
-        artistPic: 'https://codehs.com/uploads/1128f7b2a2b686cf82e14612bb291490',
-        duration: '4:42',
-        image: 'https://codehs.com/uploads/71466ceae7bd24e3e6b52d0c793e36de'
-    }
-];
-interface SongListScreenProps {
-  onSongPress: (song: Song) => void;
-}
-interface SongListScreenState {}
-class SongListScreen extends Component<SongListScreenProps, SongListScreenState> {
-  render() {
-      return (
-          <ScrollView style={styles.container}>
-              {song.map((item) => (
-                  <TouchableHighlight key={item.id} underlayColor="#444" style={styles.songItem} onPress={() => this.props.onSongPress(item)}>
-                      <Text style={styles.songText}>{item.title}</Text>
-                  </TouchableHighlight>
-              ))}
-          </ScrollView>
-      );
-  }
-}
-
-interface Song {
-  id: string;
-  title: string;
-  artist: string;
-  artistPic: string;
-  duration: string;
-  image: string;
-}
-
-interface SongDetailScreenProps {
-  song: Song;
-  onBackPress: () => void;
-} */
-
-/* class SongDetailScreen extends Component<SongDetailScreenProps> {
-  render() {
-     const { song, onBackPress } = this.props;
-      return (
-          <ScrollView contentContainerStyle={styles.songDetailContainer}>
-              <Image source={{uri: song.image}} style={styles.songImage} />
-              <Text style={styles.songTitle}>Song: {song.title}</Text>
-              <Text style={styles.songArtist}>Artist: {song.artist}</Text>
-              <Image source={{uri: song.artistPic}} style={styles.songImage} />
-              <Text style={styles.songDuration}>Song Length: {song.duration} minutes</Text>
-              <TouchableHighlight style={styles.backButton} underlayColor="#444" onPress={onBackPress}>
-                  <Text style={styles.backButtonText}>Back</Text>
-              </TouchableHighlight>
-          </ScrollView>
-      );
-  }
-} */
 
 function myMoney({placeholder, onValueChange}: {placeholder:string, onValueChange: (value: string | undefined) => void}) {
   return (
@@ -90,21 +22,44 @@ export default class App extends Component{
     monthlyExpenses: 0,
     daysSinceStart: 0,
     totalDaysInMonth: 0,
-  }
-    /* constructor(props: {}) {
-      super(props);
-      this.state = {
-          selectedSong: null,
-      };
-  }
-  handleSongPress = (song: Song) => {
-      this.setState({selectedSong: song});
+    isOnTrack: null
   };
-  handleBackPress = () => {
-      this.setState({selectedSong: null});
-  }; */
+  handleComparison = () => {
+    const {monthlyBudget, monthlyExpenses, daysSinceStart, totalDaysInMonth} = this.state;
+    const expensesRatio = monthlyBudget > 0 ? monthlyExpenses/monthlyBudget : 0;
+    const daysRatio = totalDaysInMonth > 0 ? daysSinceStart/totalDaysInMonth : 0;
+    this.setState({isOnTrack: expensesRatio <= daysRatio});
+  };
+  
   render() {
-      return (
+    const {isOnTrack} = this.state;
+    if(isOnTrack === true){
+        return(
+            <View style={styles.container}>
+                <View style={styles.header}>
+                  <Text style={styles.headerText}>You are on track!</Text>
+                </View>
+                <View style={styles.imageContainer}>
+                    <Image source={{uri: 'https://codehs.com/uploads/0c9ae41c8dccb3b2d47659498c15d38a'}} style={styles.image}/>
+                    <Button title="Back" color='#1DB954' onPress={() => this.setState({isOnTrack: null})}/>
+                </View>
+            </View>
+        )
+    }
+    if(isOnTrack === false){
+        return(
+            <View style={styles.container}>
+                <View style={styles.header}>
+                  <Text style={styles.headerText}>You are overspending!</Text>
+                </View>
+                <View style={styles.imageContainer}>
+                    <Image source={{uri: 'https://codehs.com/uploads/5e95d2b9b22ab9f650f908cc096921f6'}} style={styles.image}/>
+                    <Button title="Back" color='#1DB954' onPress={() => this.setState({isOnTrack: null})}/>
+                </View>
+            </View>
+        )
+    }
+    return (
           <View style={styles.container}>
               <View style={styles.header}>
                   <Text style={styles.headerText}>My Monthly Financial Tracker</Text>
@@ -123,8 +78,8 @@ export default class App extends Component{
                         this.setState({monthlyExpenses: numericValue});
                     },
                 })}
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 20, paddingLeft: 10, color: 'white'}}
-                    placeholderTextColor="white"
+                <TextInput style={{height: 40, backgroundColor: 'white', borderColor: 'grey', borderWidth: 1, margin: 20, paddingLeft: 10, color: 'black', fontSize: 13}}
+                    placeholderTextColor="grey"
                     placeholder="Enter the number of days it has been since the start of the month"
                     keyboardType="numeric"
                     onChangeText={(text) => {
@@ -132,8 +87,8 @@ export default class App extends Component{
                         this.setState({daysSinceStart: numericValue});
                     }}
                 />
-                <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1, margin: 20, paddingLeft: 10, color: 'white'}}
-                    placeholderTextColor="white"
+                <TextInput style={{height: 40, backgroundColor: 'white', borderColor: 'grey', borderWidth: 1, margin: 20, paddingLeft: 10, color: 'black', fontSize: 13}}
+                    placeholderTextColor="grey"
                     placeholder="Enter how many total days are in the month"
                     keyboardType="numeric"
                     onChangeText={(text) => {
@@ -141,12 +96,7 @@ export default class App extends Component{
                         this.setState({totalDaysInMonth: numericValue});
                     }}
                 />
-              {/* /*{this.state.selectedSong ? (
-                  <SongDetailScreen song={this.state.selectedSong} onBackPress={this.handleBackPress}/>
-              ) : (
-                  <SongListScreen onSongPress={this.handleSongPress}/>
-              )
-              } */}
+                <Button title="Check Status" color='#1DB954' onPress={this.handleComparison}/>
           </View>
       );
   }
@@ -169,50 +119,15 @@ const styles = StyleSheet.create({
       color: 'black',
       fontWeight: 'bold'
   },
-  songItem: {
-      width: '100%',
-      padding: 20,
-      backgroundColor: '#1DB954',
-      marginBottom: 5,
-  },
-  songText: {
-      color: '#fff',
-      fontSize: 18,
-      textAlign: 'center',
-  },
-  songDetailContainer: {
+  imageContainer:{
+      flex: 1,
+      justifyContent: 'center',
       alignItems: 'center',
-      padding: 20,
   },
-  songImage: {
-      width: 300,
-      height: 300,
-      borderRadius: 10,
+  image: {
+      width: 500,
+      height: 500,
+      borderRadius: 50,
       marginBottom: 20,
-  },
-  songTitle: {
-      fontSize: 24,
-      color: '#fff',
-      fontWeight: 'bold',
-  },
-  songArtist: {
-      fontSize: 18,
-      color: '#bbb',
-      marginTop: 10,
-  },
-  songDuration: {
-      fontSize: 16,
-      color: '#ddd',
-      marginTop: 5,
-  },
-  backButton: {
-      marginTop: 20,
-      padding: 10,
-      backgroundColor:'#1DB954',
-      borderRadius: 5,
-  },
-  backButtonText: {
-      color: '#fff',
-      fontSize: 16,
   },
 });
